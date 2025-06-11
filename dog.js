997b172f853de002986523ff5823197e37c41d12
Matheus Gonzalez <matheusverissimogonzalez@gmail.com>
2025-06-10 20:10:53 -0700

    Fixed the grid items

--- dog.js
+++ dog.js
@@ -2,17 +2,20 @@ const canvas = document.getElementById("dogCanvas");
 const ctx = canvas.getContext("2d");
 
 const sprite = new Image();
-sprite.src = "assets/Walk.png"; 
+sprite.src = "assets/Idle.png"; 
 
 const frameWidth = 48;
 const frameHeight = 48;
-const totalFrames = 6;
+const totalFrames = 4;
 
 let currentFrame = 0;
 let x = 0;
 let frameDelay = 100; 
 
 sprite.onload = () => {
+    canvas.width = frameWidth * 5;
+    canvas.height = frameHeight * 5;
+    ctx.scale(5, 5); 
     setInterval(() => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
